3c1f69f03d2275d30aa509858408d4c3ddf0918e
mustafacecoev <122642866+mustafacecoev@users.noreply.github.com>
2023-01-13 23:01:39 +0300

    Add files via upload

---
+++ RestExample/src/main/java/org/evil/domain/UserReserve.java
@@ -0,0 +1,32 @@
+package org.evil.domain;
+
+public class UserReserve {
+    private Cars cars;
+    private int quantity;
+
+    public UserReserve() {
+        this.cars = new Cars();
+        this.quantity = 0;
+    }
+
+    public UserReserve(Cars cars, int quantity) {
+        this.cars = cars;
+        this.quantity = quantity;
+    }
+
+    public Cars getClothes() {
+        return cars;
+    }
+
+    public void setClothes(Cars cars) {
+        this.cars = cars;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
